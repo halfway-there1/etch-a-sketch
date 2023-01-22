f4ee9163afcb2f61611bb021e2570b7ea010214c
Anurag Kumar <anuragkushwaahan@gmail.com>
2023-01-22 10:25:03 +0530

    Refactor javascript code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
 
-const n = 16;
+const grid_size = 320; // px
+const n = 16; // no. of sqrs per side of the grid
+const sqr_size = grid_size / n;
 for (let i = 0; i < n * n; i++) {
-  const size = 320 / n;
-
   const sqr = document.createElement('div');
-  sqr.style.cssText = `width:${size}px; height:${size}px`;
+  sqr.style.cssText = `width: ${sqr_size}px; height: ${sqr_size}px`;
   sqr.addEventListener('mouseover', () => {
     sqr.classList.add('hover');
   });
